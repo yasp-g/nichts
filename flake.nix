d97f51bcb9b33fee812171c4e94fd79088839f5b
yasp-g <jasper@yasp.studio>
2026-02-18 11:34:18 +0100

    feat: add mini host and darwin package to flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,24 @@
 {
-  description = "NixOS configuration for personal fleet";
+  description = "Nichts here to see.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # macOS system management (Phase 6)
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = { self, nixpkgs, home-manager, nix-darwin }: {
+
+    # NixOS hosts
     nixosConfigurations.mbp2015 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -24,5 +33,22 @@
         }
       ];
     };
+
+    # macOS hosts (uncomment when ready)
+    # darwinConfigurations.mini = nix-darwin.lib.darwinSystem {
+    #   system = "aarch64-darwin";
+    #   modules = [
+    #     ./hosts/mini
+    #     home-manager.darwinModules.home-manager
+    #     {
+    #       home-manager = {
+    #         useGlobalPkgs = true;
+    #         useUserPackages = true;
+    #         users.yasp = import ./users/yasp/home.nix;
+    #       };
+    #     }
+    #   ];
+    # };
+
   };
 }
